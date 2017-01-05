ec40ae34b5ce4ed9de916129602abfe6d827e8ef
bhattachansamrat <bhattachansamrat@gmail.com>
2017-01-05 12:00:28 +0545

    spa ng-view started

--- tutorial/Tutorial/HomeCinema.Web/App_Start/WebApiConfig.cs
+++ tutorial/Tutorial/HomeCinema.Web/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using HomeCinema.Web.Infrastructure.MessageHandlers;
 
 namespace HomeCinema.Web
 {
@@ -9,6 +10,7 @@ namespace HomeCinema.Web
     {
         public static void Register(HttpConfiguration config)
         {
+            config.MessageHandlers.Add(new HomeCinemaAuthHandler());
             // Web API configuration and services
 
             // Web API routes
